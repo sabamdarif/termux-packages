9c4da7bd180101063e815f833c001098b4c3e226
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2026-06-11 12:29:52 +0530

    tree-wide: GNOME Desktop

--- packages/libical/build.sh
+++ packages/libical/build.sh
@@ -2,28 +2,47 @@ TERMUX_PKG_HOMEPAGE=https://libical.github.io/libical/
 TERMUX_PKG_DESCRIPTION="Libical is an Open Source implementation of the iCalendar protocols and protocol data units"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.0.2"
+TERMUX_PKG_VERSION="3.0.20"
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/libical/libical/releases/download/v$TERMUX_PKG_VERSION/libical-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=39a979bb5474af3c4601c83dc71512c1121dee56722b3d8d8668ed75bd78ccdd
+TERMUX_PKG_SHA256=e73de92f5a6ce84c1b00306446b290a2b08cdf0a80988eca0a2c9d5c3510b4c2
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, libicu"
+TERMUX_PKG_DEPENDS="glib, libc++, libicu, libxml2"
 TERMUX_PKG_BREAKS="libical-dev"
 TERMUX_PKG_REPLACES="libical-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--DLIBICAL_BUILD_DOCS=false
--DLIBICAL_GLIB=false
--DLIBICAL_GOBJECT_INTROSPECTION=false
+-DSHARED_ONLY=true
 -DUSE_BUILTIN_TZDATA=true
 -DPERL_EXECUTABLE=/usr/bin/perl
+-DICAL_GLIB=true
+-DIMPORT_ICAL_GLIB_SRC_GENERATOR=$TERMUX_PKG_HOSTBUILD_DIR/host-prefix/lib/cmake/LibIcal/IcalGlibSrcGenerator.cmake
+-DENABLE_GTK_DOC=false
 "
+TERMUX_PKG_HOSTBUILD=true
+
+termux_step_host_build() {
+	termux_setup_cmake
+	termux_setup_ninja
+
+	mkdir -p "$TERMUX_PKG_HOSTBUILD_DIR"/host-prefix
+
+	cmake \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_INSTALL_PREFIX="$TERMUX_PKG_HOSTBUILD_DIR"/host-prefix \
+		-DENABLE_GTK_DOC=OFF \
+		-G Ninja \
+		"$TERMUX_PKG_SRCDIR"
+	cmake --build .
+	cmake --install . --prefix "$TERMUX_PKG_HOSTBUILD_DIR"/host-prefix
+}
 
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=4
+	local _SOVERSION=3
 
-	local v=$(sed -En 's/^set\(LIBICAL_LIB_MAJOR_SOVERSION\s+"?([0-9]+).*/\1/p' \
-			CMakeLists.txt)
+	local v=$(sed -En 's/^set\(LIBICAL_LIB_MAJOR_VERSION\s+"?([0-9]+).*/\1/p' \
+		CMakeLists.txt)
 	if [ "${v}" != "${_SOVERSION}" ]; then
 		termux_error_exit "SOVERSION guard check failed."
 	fi
