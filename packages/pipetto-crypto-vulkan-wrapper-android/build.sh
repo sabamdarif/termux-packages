071e142c08be77395250b27a4946df09258d77d6
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2026-08-25 23:24:35 +0530

    addpkg(main/pipetto-crypto-vulkan-wrapper-android): 25.0.0

---
+++ packages/pipetto-crypto-vulkan-wrapper-android/build.sh
@@ -0,0 +1,60 @@
+TERMUX_PKG_HOMEPAGE=https://www.mesa3d.org
+TERMUX_PKG_DESCRIPTION="Android Vulkan ICD"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE_FILE="docs/license.rst"
+TERMUX_PKG_MAINTAINER="xMeM <haooy@outlook.com>"
+TERMUX_PKG_VERSION="25.0.0"
+TERMUX_PKG_REVISION=2
+TERMUX_PKG_SRCURL=git+https://github.com/Pipetto-crypto/mesa
+TERMUX_PKG_GIT_BRANCH=wrapper-25
+_COMMIT=7eae6442f5d8a7414e66adc0d42857c143f20fa9
+TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libdrm, libx11, libxcb, libxshmfence, libwayland, vulkan-loader-generic, zlib, zstd"
+TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols, libxrandr, xorgproto"
+TERMUX_PKG_API_LEVEL=26
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--cmake-prefix-path $TERMUX_PREFIX
+-Dcpp_rtti=false
+-Dgbm=disabled
+-Dopengl=false
+-Dllvm=disabled
+-Dshared-llvm=disabled
+-Dplatforms=x11
+-Dgallium-drivers=
+-Dxmlconfig=disabled
+-Dvulkan-drivers=wrapper
+-Db_ndebug=true
+"
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout $_COMMIT
+	# Do not use meson wrap projects
+	# rm -rf subprojects
+}
+
+termux_step_pre_configure() {
+	termux_setup_cmake
+
+	if [ "$TERMUX_ON_DEVICE_BUILD" = "true" ]; then
+		CFLAGS+=" --target=$TERMUX_HOST_PLATFORM$TERMUX_PKG_API_LEVEL"
+	fi
+
+	CPPFLAGS+=" -D__USE_GNU"
+	LDFLAGS+=" -landroid-shmem"
+
+	_WRAPPER_BIN=$TERMUX_PKG_BUILDDIR/_wrapper/bin
+	mkdir -p $_WRAPPER_BIN
+	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
+		sed 's|@CMAKE@|'"$(command -v cmake)"'|g' \
+			$TERMUX_PKG_BUILDER_DIR/cmake-wrapper.in \
+			>$_WRAPPER_BIN/cmake
+		chmod 0700 $_WRAPPER_BIN/cmake
+		termux_setup_wayland_cross_pkg_config_wrapper
+	fi
+	export PATH=$_WRAPPER_BIN:$PATH
+}
+
+termux_step_post_configure() {
+	rm -f $_WRAPPER_BIN/cmake
+}
